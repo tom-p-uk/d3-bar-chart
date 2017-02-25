a49eda0fbf59924910e5bb77417201f74ba0ba14
Tom Price <tom_p@hotmail.co.uk>
2017-02-25 18:30:05 +0900

    Set up basic bar chart

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,62 +2,46 @@
 
 const renderBarChart = data => {
 
-  const width = 500;
+
+  console.log(data);
+  const width = 900;
   const height = 500;
   const padding = 20;
-  const barWidth = 50;
+  const margin = { top: 20, right: 20, bottom: 40, left: 40 };
 
-  // const data = [[146, 'a'], [255, 'a'], [440, 'a'], [95, 'a'], [74, 'a']];
+  let barWidth = width / data.length;
+  barWidthPadding = barWidth * 0.5;
+  barWidth = barWidth - barWidthPadding;
 
-
-  const xMin = 0;
-  const xMax = d3.max(data, (d) => d.length * barWidth);
-  const yMin = d3.min(data, (d) => d[0]);
-  const yMax = d3.max(data, (d) => d[0]);
-
-  const xScale = d3.scaleLinear()
-                   .domain([0, data.length])
-                   .range([padding, width - padding]);
-
-  // const xScale = d3.scaleLinear()
-  //                 .domain([0, xMax])
-  //                 .scale([padding, width - padding]);
-
-  // const yScale = d3.scaleLinear()
-  //                 .domain([0, yMax])
-  //                 .scale([height - padding, padding])
-
-  const yScale = d3.scaleLinear()
-                  .domain([0, d3.max(data, (d) => d[0])])
-                  .range([height - padding, padding]);
+  const dataMax = d3.max(data, (d) => d[1]);
 
   const svg = d3.select('.container')
     .append('svg')
     .attr('width', width)
     .attr('height', height)
-    .attr('fill', 'gray')
 
-  svg.selectAll('rect')
+  const xScale = d3.scaleBand()
+    .domain(data.map((d) => d[0]))
+    .range([margin.left, width - margin.right])
+    .paddingInner(0.1)
+
+  const yScale = d3.scaleLinear()
+    .domain([0, dataMax])
+    .range([margin.bottom, height - margin.top])
+
+
+  const bars = svg.selectAll('rect')
     .data(data)
     .enter()
     .append('rect')
-    .attr('class', 'bar')
-    .attr('width', 45)
-    .attr('height', (d) => d[0])
-    .attr('padding', 10)
-    .attr('fill', 'navy')
-    .attr('x', (d, i) => xScale(i))
-    .attr('y', (d) => yScale(d[0]))
-    .attr('data-date', (d) => '1')
-    .attr('data-gdp', (d) => '2')
-
-  // svg.selectAll('text')
-  //   .data(dataSet)
-  //   .append('text')
-  //   .attr('x',)
+    .attr('height', (d) => yScale(d[1]) - margin.bottom)
+    .attr('x', (d, i) => xScale(d[0]))
+    .attr('y', (d) => height - yScale(d[1]))
+    .attr('fill', 'blue')
+    .attr('width', barWidth)
 
 };
 
-$.getJSON('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', (data) => console.log(data));
-const data = [[146, 'a'], [255, 'a'], [440, 'a'], [95, 'a'], [74, 'a']];
-renderBarChart(data);
+$.getJSON('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', (data) => renderBarChart(data.data));
+const data = [[146, 'a'], [255, 'b'], [440, 'c'], [95, 'd'], [74, 'e']];
+// renderBarChart(data);
